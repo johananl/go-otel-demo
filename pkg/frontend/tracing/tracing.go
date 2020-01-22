b2b06a1e90f5ef1233c583a776c81af7a4eb717d
Johannes Liebermann <johanan.liebermann@gmail.com>
2020-01-22 11:20:55 +0100

    Remove unused code

--- pkg/frontend/tracing/tracing.go
+++ pkg/frontend/tracing/tracing.go
@@ -3,12 +3,9 @@ package tracing
 import (
 	"context"
 
-	"go.opentelemetry.io/otel/api/trace"
 	"go.opentelemetry.io/otel/plugin/grpctrace"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 )
 
 // UnaryClientInterceptor intercepts and injects outgoing trace data.
@@ -20,17 +17,6 @@ func UnaryClientInterceptor(ctx context.Context, method string, req, reply inter
 	ctx = metadata.NewOutgoingContext(ctx, metadataCopy)
 
 	err := invoker(ctx, method, req, reply, cc, opts...)
-	setTraceStatus(ctx, err)
 
 	return err
-}
-
-func setTraceStatus(ctx context.Context, err error) {
-	if err != nil {
-		s, _ := status.FromError(err)
-		trace.SpanFromContext(ctx).AddEvent(ctx, err.Error())
-		trace.SpanFromContext(ctx).SetStatus(s.Code())
-	} else {
-		trace.SpanFromContext(ctx).SetStatus(codes.OK)
-	}
 }
